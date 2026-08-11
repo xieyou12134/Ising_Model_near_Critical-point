faea637a3b85b4b0f869f2e156c1b64e76d8a9da
xieyou12134 <1245864208@qq.com>
2026-08-11 07:33:31 +0000

    Remove small-scale critical training contents

--- 3-Small_Scale_Critical_Training/scripts/sample.py
+++
@@ -1,4 +0,0 @@
-from ising_scale_diffusion.cli import sample_main
-
-if __name__ == "__main__":
-    raise SystemExit(sample_main())
--- 3-Small_Scale_Critical_Training/scripts/train.py
+++
@@ -1,4 +0,0 @@
-from ising_scale_diffusion.cli import train_main
-
-if __name__ == "__main__":
-    raise SystemExit(train_main())
